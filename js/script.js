70ec98761b7b1e7ab8baf0f564b338b498b5492f
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-01-12 00:56:47 -0300

    costa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector("#menu-btn")
+document.getElementById('menu-btn').onclick = () => {
+    navbar.classList.add('active');
+}
+
+document.getElementById('nav-close').onclick = () => {
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.getElementById('search-btn').onclick = () => {
+    searchForm.classList.add('active');
+}
+
+document.getElementById('close-search').onclick = () => {
+    searchForm.classList.remove('active');
+}
